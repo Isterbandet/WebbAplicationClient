44cd7de9a81c5014c9cb21f8090b0a8355aa1c97
Anton <anton_eklof@hotmail.com>
2018-03-15 22:45:06 +0100

    Gjort en jävla msasa

--- src/main/java/com/mycompany/webbaplicatoncomputer/JSONKompisClient.java
+++ src/main/java/com/mycompany/webbaplicatoncomputer/JSONKompisClient.java
@@ -3,6 +3,7 @@ package com.mycompany.webbaplicatoncomputer;
 
 import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
@@ -16,23 +17,86 @@ public class JSONKompisClient {
     private static ClientConfig config = new DefaultClientConfig();
     private static Client client = Client.create(config);
     private static WebResource service = client.resource(
-            UriBuilder.fromUri("http://localhost:8080/RestFull/rest/KompisarServiceX").build());
+            UriBuilder.fromUri("http://localhost:8080/RestFull").build());
     
-    public static void main(String[] args) throws IOException {
+   
+    private void sökKompis(String namn){
         //Hämta en kompis!
-        String jsonKompis = service.path("kompisar/Fredde")
+        String jsonKompis = service.path("rest/KompisarServiceX/kompisarJSON/" + namn)
                 .accept(MediaType.APPLICATION_JSON).get(String.class);
         System.out.println(jsonKompis);
         System.out.println();
         
         Kompisar kompis = new Gson().fromJson(jsonKompis, Kompisar.class);
+        System.out.println("Kompisens namn ;" + kompis.getNamn() + " nummer: "
+        +kompis.getNummer()+ "\n");
+        //return System.out.println("Succesfulkl");
+    }
+    public void addaEnKompis(){
+        //Adda en kompis med post
+       Kompisar kompis = new Kompisar("Rövungen", 133);
+       
+       ClientResponse response = service.path("rest/KompisarServiceX/kompisar/add")
+               .accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, kompis);
+       System.out.println("Response" + response.getEntity(String.class));
+    }
+    
+    public void listaKompisar(){
+         Gson gson = new Gson();
+                
+        //Hämta alla kompsiar
+        String jsonString = service.path("rest/KompisarServiceX/kompisarJSON")
+        .accept(MediaType.APPLICATION_JSON).get(String.class);
+        
+        Kompisar[] kompis = gson.fromJson(jsonString, Kompisar[].class);
+        for (Kompisar k : kompis){
+            System.out.println("Person" + k.getNamn() + "Nummreva" + k.getNummer() );
+        }
+    }
+    public void tabortKompis(String kompis){
+        //Ta bort en polare
+        String jsonKompis = service.path("rest/KompisarServiceX/kompisarJSON/"+kompis+"/delete")
+                .accept(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println(kompis +"borttagen");
         
     
     
     
     }
+    
+    
+    
+    
+    public static void main(String[] args) throws IOException {
+        
+       
+
+        JSONKompisClient kompis = new JSONKompisClient();
+        kompis.sökKompis("Freddan");//1
+        
+        kompis.listaKompisar();//5
+        
+        kompis.tabortKompis("Freddan");//4
+        kompis.listaKompisar();//5
+        
+    
+        
+        
+        
+        
+        
+        
+       
+        
+    
     }
+    
 }
+  
+   
+    
+    
+
     
     
 
