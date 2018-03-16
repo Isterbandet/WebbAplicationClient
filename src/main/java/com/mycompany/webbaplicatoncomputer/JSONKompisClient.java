15f994f1ba4764e462d44f0707885597044caf46
Anton Eklöf <anton_eklof@hotmail.com>
2018-03-16 23:42:01 +0100

    Gjort massor med filerna

--- src/main/java/com/mycompany/webbaplicatoncomputer/JSONKompisClient.java
+++ src/main/java/com/mycompany/webbaplicatoncomputer/JSONKompisClient.java
@@ -79,7 +79,7 @@ public class JSONKompisClient {
         
         kompis.tabortKompis("Freddan");//4
         kompis.listaKompisar();//5
-        kompis.addaEnKompis();
+        //kompis.addaEnKompis();
         
     
         
